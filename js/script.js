1d6166009f30d8af6c6895b78f913b598ea616bb
PerfectELK <35468296+PerfectELK@users.noreply.github.com>
2018-02-01 00:08:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -152,3 +152,109 @@ setInterval(function(){
 	$(".layer3").css("box-shadow","");
 	},10000);
 },15000);
+
+/* Модалочка, никаких библиотек, как и просили */
+
+function showModalWin() {
+
+    var fixed = document.body;
+    var button = document.getElementById('button');
+    var darkLayer = document.createElement('div'); 
+    darkLayer.id = 'shadow'; 
+    document.body.appendChild(darkLayer); 
+    var modalWin = document.getElementById('popupWin'); 
+    modalWin.style.display = 'inline-block'; 
+    fixed.style.overflow = "hidden";
+    fixed.style.marginLeft = "-17px";
+    darkLayer.onclick = function () {  
+    darkLayer.parentNode.removeChild(darkLayer); 
+    modalWin.style.display = 'none'; 
+    fixed.style.overflow = "auto";
+    fixed.style.borderLeft = "0";
+    fixed.style.marginLeft = "0";
+    return false;
+    };
+
+    button.onclick = function(){
+    darkLayer.parentNode.removeChild(darkLayer);
+    modalWin.style.display = 'none';
+    fixed.style.overflow = "auto";
+    fixed.style.borderLeft = "0";
+    fixed.style.marginLeft = "0";
+    return false;
+    };
+}
+var cls1 = document.getElementById("col-6-1");
+var cls2 = document.getElementById("col-6-2");
+var cls3 = document.getElementById("col-6-3");
+var cls4 = document.getElementById("col-6-4");
+var cls5 = document.getElementById("col-6-5");
+var cls6 = document.getElementById("col-6-6");
+var cls7 = document.getElementById("col-6-img1");
+var cls8 = document.getElementById("col-6-img2");
+var cls9 = document.getElementById("col-6-img3");
+var cls10 = document.getElementById("col-6-7");
+var cls11 = document.getElementById("col-6-8");
+var cls12 = document.getElementById("col-6-9");
+var cls13 = document.getElementById("col-6-10");
+var cls14 = document.getElementById("col-6-11");
+var cls15 = document.getElementById("col-6-12");
+var cls16 = document.getElementById("col-6-foot1");
+var cls17 = document.getElementById("col-6-foot2");
+var cls18 = document.getElementById("col-6-foot3");
+var cls19 = document.getElementById("col-6-foot4");
+
+var html = document.documentElement;
+
+setInterval (function (){
+if (html.clientWidth <= 1150){ 
+ cls1.className = "col-12 dish1";
+ cls2.className = "col-12 dish1-price one";
+ cls3.className = "col-12 dish1-price";
+ cls4.className = "col-12 dish2";
+ cls5.className = "col-12 dish1 dish3";
+ cls6.className = "col-12 dish1-price";
+ cls7.className = "col-12 alione-img imgAlione1";
+ cls8.className = "col-12 alione-img imgAlione2";
+ cls9.className = "col-12 alione-img imgAlione3";
+ cls10.className = "col-12 Speisekarte1";
+ cls11.className = "col-12 Speisekarte1";
+ cls12.className = "col-12 Speisekarte1";
+ cls13.className = "col-12 Speisekarte1";
+ cls14.className = "col-12 Kontakt form";
+ cls15.className = "col-12 Kontakt google";
+ cls16.className = "col-12 footer-img";
+ cls17.className = "col-12 footer-img";
+ cls18.className = "col-12 footer-img";
+ cls19.className = "col-12 footer-img";
+} else{
+    cls1.className = "col-6 dish1";
+    cls2.className = "col-6 dish1-price one";
+    cls3.className = "col-6 dish1-price";
+    cls4.className = "col-6 dish2";
+    cls5.className = "col-6 dish1 dish3";
+    cls6.className = "col-6 dish1-price";
+    cls7.className = "col-3 alione-img imgAlione1";
+    cls8.className = "col-3 alione-img imgAlione1";
+    cls9.className = "col-3 alione-img imgAlione1";
+    cls10.className = "col-6 Speisekarte1";
+    cls11.className = "col-6 Speisekarte1";
+    cls12.className = "col-6 Speisekarte1";
+    cls13.className = "col-6 Speisekarte1";
+ 	cls14.className = "col-6 Kontakt form";
+ 	cls15.className = "col-6 Kontakt google";
+ 	cls16.className = "col-3 footer-img";
+ 	cls17.className = "col-3 footer-img";
+ 	cls18.className = "col-3 footer-img";
+ 	cls19.className = "col-3 footer-img";
+
+}
+}, 500); 
+$(document).ready(function () {
+    $("a").click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+            $('html').animate({ scrollTop: destination }, 1100);
+        return false; 
+    });
+});
